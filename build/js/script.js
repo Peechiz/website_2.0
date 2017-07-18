f07d234735516ce503197723791e30114f8e7ad3
Christopher Impicciche <cimpicci@gmail.com>
2017-07-18 13:49:43 -0500

    adds script.js

---
+++ build/js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+
+  //  highlite the appropriate nav link
+  const full = window.location.href.split('/');
+  let root = full[full.length-1];
+  root = root.slice(0,root.length-5);
+  console.log(root);
+
+  let page = document.querySelector(`#${root}`);
+  console.log(page);
+  page.classList.add('active');
+
+
+  // hamburger
+  let burger = document.querySelector('#hamburger');
+  let header = document.querySelector('header');
+  burger.addEventListener('click', () => {
+    header.classList.toggle('closed')
+  })
+});
